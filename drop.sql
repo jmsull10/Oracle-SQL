4012173b7b62c13a1a4b2ffedd13cbc4105fb560
jmsull10 <88500377+jmsull10@users.noreply.github.com>
2021-08-05 09:23:14 -0700

    Add files via upload

---
+++ drop.sql
@@ -0,0 +1,12 @@
+/*
+    Justin Sullivan, ACO320
+    SQL Oracle assignment
+    18 November, 2020
+*/
+drop table document;
+drop table internal;
+drop table external;
+drop table displays;
+drop table webpage;
+drop table site;
+drop table graphic;
